509348e83508db8f3c2620eb640a207edeaf8c9d
X-88 <126267638+MyBooty165@users.noreply.github.com>
2023-11-30 17:22:14 -0600

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,14 +1,11 @@
 $(document).ready(function () {
     const textToType = "Tam's Alternative Project";
-    const terminal = $(".terminal");
     const typedText = $("#typed-text");
     const subText = $("#sub-text");
     const leftButton = $(".left-btn");
     const rightButton = $(".right-btn");
 
     setTimeout(function () {
-        terminal.find(".cursor").remove();
-        typedText.text("");
         typeText(0);
     }, 1500);
 
@@ -21,16 +18,13 @@ $(document).ready(function () {
         } else {
             typedText.addClass("finished");
             subText.removeClass("hidden");
+            leftButton.removeClass("hidden");
+            rightButton.removeClass("hidden");
+
             setTimeout(function () {
-                leftButton.removeClass("hidden");
-                rightButton.removeClass("hidden");
-                setTimeout(function () {
-                    leftButton.addClass("zoom-out");
-                }, 50);
-                setTimeout(function () {
-                    rightButton.addClass("zoom-out");
-                }, 100); 
-            }, 500);
+                leftButton.addClass("zoom-out");
+                rightButton.addClass("zoom-out");
+            }, 50);
         }
     }
 });
